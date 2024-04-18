e280ca3b398acfed2e5a7effe67a45865db6a89b
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2024-04-18 02:58:56 +0200

    Upgrade to dotnet 9.
    Replace SwaggerGen with OpenApi.
    Use minimal APIs.

--- AspNetCore.Examples.OpenTelemetry.Api/Program.cs
+++ AspNetCore.Examples.OpenTelemetry.Api/Program.cs
@@ -1,31 +1,22 @@
 using AspNetCore.Examples.OpenTelemetry.Api.Extensions;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging
     .AddCustomOpenTelemetry(builder.Configuration);
 
-// Add services to the container.
 builder.Services
-    .AddControllers()
-    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()))
-    .Services
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGen()
+    .AddCustomOpenApi()
     .AddCustomOpenTelemetry(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapCustomOpenApi();
+    app.UseCustomOpenApiUI();
 }
 
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapTelemetryEndpoints();
 
 app.Run();
