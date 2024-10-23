5e7d472236aa81326bb9db9f6b87e0b1cdf4c74d
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2024-10-23 13:01:57 +0200

    Improve instrument name.

--- AspNetCore.Examples.OpenTelemetry.Api/WeatherForecast/Services/WeatherForecastTelemetry.cs
+++ AspNetCore.Examples.OpenTelemetry.Api/WeatherForecast/Services/WeatherForecastTelemetry.cs
@@ -15,7 +15,7 @@ internal class WeatherForecastTelemetry : Telemetry<WeatherForecast>, IWeatherFo
     public WeatherForecastTelemetry(ILoggerFactory loggerFactory, IMeterFactory meterFactory)
         : base(loggerFactory, meterFactory)
     {
-        TemperatureC = Meter.CreateHistogram<int>("temperature", unit: "ºC");
+        TemperatureC = Meter.CreateHistogram<int>("weather_forecast.temperature", unit: "ºC");
     }
 
     public Histogram<int> TemperatureC { get; }
