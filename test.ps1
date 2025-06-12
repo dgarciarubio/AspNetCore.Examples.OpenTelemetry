ddb93daac67f7808262ba42b83491783269d3f29
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2025-06-12 11:13:42 +0200

    Simplify test script.

--- test.ps1
+++ test.ps1
@@ -1,18 +1,7 @@
-param(
-	[switch] $RunBenchmarks = $false
-	[string] $Configuration = "Release"
-) 
-
 Get-ChildItem -Path . -Directory -Filter TestResults -Recurse | 
   Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
 
-dotnet test ./TelemetryServices.Tests/                --configuration $Configuration -- --coverage --coverage-output "coverage.cobertura.xml" --coverage-output-format "cobertura"
-dotnet test ./TelemetryServices.OpenTelemetry.Tests/  --configuration $Configuration -- --coverage --coverage-output "coverage.cobertura.xml" --coverage-output-format "cobertura"
-
-if ($RunBenchmarks)
-{
-	dotnet test ./TelemetryServices.Benchmarks.Tests/ --configuration Release
-}
+dotnet test -- --coverage --coverage-output "coverage.cobertura.xml" --coverage-output-format "cobertura"
 
 dotnet tool update dotnet-reportgenerator-globaltool --global --version 5.4.6
 reportgenerator `
